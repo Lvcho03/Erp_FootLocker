507904653dade103665789a33c42b55a6cd256cc
Lvcho03 <luchomirandatwo@gmail.com>
2024-10-31 14:31:39 +0100

    Ultimo cambio del main

--- Web Footlocker/JS/fadein.js
+++ Web Footlocker/JS/fadein.js
@@ -1,13 +1,20 @@
+// formulario.js
 
-
-function cerrarFormulario() {
-    const formContainer = document.getElementById('contenedor-form');
-    formContainer.classList.remove('in');
-    formContainer.style.display = 'none'; // Oculta el contenedor completamente
-}
 function mostrarFormulario() {
-    const formContainer = document.getElementById('contenedor-form');
-    formContainer.classList.add('in');
-    formContainer.classList.add('sombra');
-    formContainer.style.display = 'flex';
+    document.getElementById('contenedor-form').style.display = 'block';
 }
+
+function cerrarFormulario(event) {
+    if (event) event.stopPropagation(); // Evitar cerrar al hacer clic en el modal
+    document.getElementById('contenedor-form').style.display = 'none';
+}
+
+// Espera que la página se cargue completamente
+window.addEventListener('load', function() {
+    const titulo = document.getElementById('titulo');
+    
+    // Agrega la clase 'mostrar' después de un breve retraso
+    setTimeout(() => {
+        titulo.classList.add('mostrar');
+    }, 500); // Aparece después de 0.5 segundos
+});
