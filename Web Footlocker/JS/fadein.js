a0c51c8783309371ddac05bc246468396e7dd788
Lvcho03 <luchomirandatwo@gmail.com>
2024-10-31 12:21:34 +0100

    cambioFinal

--- Web Footlocker/JS/fadein.js
+++ Web Footlocker/JS/fadein.js
@@ -1,11 +1,13 @@
-function mostrarFormulario() {
-    const formContainer = document.getElementById('contenedor-form');
-    formContainer.classList.add('in');
-    formContainer.style.display = 'flex';
-}
+
 
 function cerrarFormulario() {
     const formContainer = document.getElementById('contenedor-form');
     formContainer.classList.remove('in');
     formContainer.style.display = 'none'; // Oculta el contenedor completamente
+}
+function mostrarFormulario() {
+    const formContainer = document.getElementById('contenedor-form');
+    formContainer.classList.add('in');
+    formContainer.classList.add('sombra');
+    formContainer.style.display = 'flex';
 }
